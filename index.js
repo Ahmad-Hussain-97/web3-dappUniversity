8515373968ea2d900c3fe8e88b2782bfa642bd8c
Ahmad Hussain <63265101+Ahmad-Hussain-97@users.noreply.github.com>
2021-03-24 21:56:19 +0500

    Task 2

--- index.js
+++ index.js
@@ -5,13 +5,87 @@ var Tx = require("ethereumjs-tx");
 
 //Task 1: Connect and check account balance
 function checkAccountBalance() {
-  const account_address = "0x657ABc323D94452ca8c79F8c36AC69440681ab89";
+  const account_address = "0x687422eEA2cB73B5d3e242bA5456b782919AFc85";
   web3.eth.getBalance(account_address, (err, wei) => {
     if (err) {
-      console.log("Error while checking balance:", err);
+      console.log(">>>>>Error:", err);
     } else {
       balance = web3.utils.fromWei(wei, "ether");
-      console.log("Account balance retrieved:", balance);
+      console.log("<<<<<Balance:", balance);
     }
+  });
+}
+
+//Task 2: Accessing smart contracts
+function callViewState() {
+  const contract_abi = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "newAge",
+          type: "uint256",
+        },
+      ],
+      name: "setAge",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "newName",
+          type: "string",
+        },
+      ],
+      name: "setName",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "getAge",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getName",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+
+  const contract_address = "0xae9c87CBED76cD0243077D6483bB8257bb8bE450";
+  const contract = new web3.eth.Contract(contract_abi, contract_address);
+
+  contract.methods.getAge().call((err, result) => {
+    if (err) {
+      console.log(">>>>>Error:", err);
+      return;
+    }
+    console.log("<<<<<Age:", result);
   });
 }
